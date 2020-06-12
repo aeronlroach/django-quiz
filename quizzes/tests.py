badcf3156c11542bf99e821f779fc99929b128b7
Aeron Roach <roacha@oregonstate.edu>
2020-06-12 01:24:30 -0700

    Updating testing functionality

--- quizzes/tests.py
+++ quizzes/tests.py
@@ -5,11 +5,37 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
+from django.test.client import RequestFactory
 
-from .models import Quiz
+from .models import Quiz, Category, Question, Answer, Feedback, UserResponse
+from .views import create_user_response, save_user_feedback, feedback, get_feedback_pdf
+
 
 # Built following alongside Django Software Foundation's Writing your first Django app Tutorial
 # https://docs.djangoproject.com/en/3.0/intro/tutorial01/
+
+
+def create_quiz(quiz_name, days, active_level):
+    """
+    Create a quizzes with the given `quiz_name`, created the
+    given number of `days` offset to now (negative for quizzes published
+    in the past, positive for quizzes that have yet to be published).
+    "active_level" is a boolean, with False meaning Inactive and True meaning active.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    quiz = Quiz.objects.create(name=quiz_name, pub_date=time, active_quiz=active_level)
+    category = Category.objects.create(parent_quiz=quiz, category_name="test catergory")
+    question = Question.objects.create(parent_quiz=quiz, parent_category=category,
+                                       question_text="How's this test question?")
+    answer_1h = Answer.objects.create(parent_quiz=quiz, parent_category=category, parent_question=question,
+                                      answer_text="Great!", answer_weight=1)
+    answer_1l = Answer.objects.create(parent_quiz=quiz, parent_category=category, parent_question=question,
+                                      answer_text="BOO!", answer_weight=0)
+    feedback_1h = Feedback.objects.create(parent_quiz=quiz, parent_category=category, parent_question=question,
+                                          parent_answer=answer_1h, feedback_text="No feedback")
+    feedback_1l = Feedback.objects.create(parent_quiz=quiz, parent_category=category, parent_question=question,
+                                          parent_answer=answer_1l, feedback_text="Give up, it be hopeless")
+    return quiz, category, question, answer_1h, answer_1l, feedback_1h, feedback_1l
 
 
 class QuizModelTests(TestCase):
@@ -41,29 +67,19 @@ class QuizModelTests(TestCase):
         recent_quiz = Quiz(pub_date=time)
         self.assertIs(recent_quiz.was_published_recently(), True)
 
-    def test_active(self):
+    def test_awake(self):
         """
          check_active() returns True for active quizzes
         """
         activated_quiz = Quiz(active_quiz=True)
-        self.assertIs(activated_quiz.check_active(),True)
+        self.assertIs(activated_quiz.check_active(), True)
 
-    def test_inactive(self):
+    def test_sleep(self):
         """
         check_active() returns False for inactive quizzes
         """
         inactive_quiz = Quiz(active_quiz=True)
-        self.assertIs(inactive_quiz.check_active(),True)
-
-
-def create_quiz(quiz_name, days):
-    """
-    Create a quizzes with the given `quiz_name` and published the
-    given number of `days` offset to now (negative for quizzes published
-    in the past, positive for quizzes that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Quiz.objects.create(quiz_name=quiz_name, pub_date=time)
+        self.assertIs(inactive_quiz.check_active(), True)
 
 
 class QuizIndexViewTests(TestCase):
@@ -76,71 +92,96 @@ class QuizIndexViewTests(TestCase):
         self.assertContains(response, "No quizzes are available.")
         self.assertQuerysetEqual(response.context['latest_quiz_list'], [])
 
-    def test_past_quiz(self):
+    def test_awake_quiz(self):
         """
-        Quizzes with a pub_date in the past are displayed on the
-        index page.
+        Active Quizzes are displayed on the index page.
         """
-        create_quiz(quiz_name="Past quizzes.", days=-30)
+        create_quiz(quiz_name="Awake quizzes.", days=-30, active_level=True)
         response = self.client.get(reverse('quizzes:index'))
         self.assertQuerysetEqual(
             response.context['latest_quiz_list'],
-            ['<Quiz: Past quizzes.>']
+            ['<Quiz: Awake quizzes.>']
         )
 
-    def test_future_quiz(self):
+    def test_sleep_quiz(self):
         """
-        Quizzes with a pub_date in the future aren't displayed on
+        Inactive quizzes aren't displayed on
         the index page.
         """
-        create_quiz(quiz_name="Future quizzes.", days=30)
+        create_quiz(quiz_name="Sleep quizzes.", days=-30, active_level=False)
         response = self.client.get(reverse('quizzes:index'))
         self.assertContains(response, "No quizzes are available.")
         self.assertQuerysetEqual(response.context['latest_quiz_list'], [])
 
-    def test_future_quiz_and_past_quiz(self):
+    def test_awake_and_sleep(self):
         """
-        Even if both past and future quizzes exist, only past quizzes
+        Even if both active and inactive quizzes exist, only active quizzes
         are displayed.
         """
-        create_quiz(quiz_name="Past quizzes.", days=-30)
-        create_quiz(quiz_name="Future quizzes.", days=30)
+        create_quiz(quiz_name="Awake quizzes.", days=-30, active_level=True)
+        create_quiz(quiz_name="Sleep quizzes.", days=-30, active_level=False)
         response = self.client.get(reverse('quizzes:index'))
         self.assertQuerysetEqual(
             response.context['latest_quiz_list'],
-            ['<Quiz: Past quizzes.>']
+            ['<Quiz: Awake quizzes.>']
         )
 
-    def test_two_past_questions(self):
+    def test_two_awake(self):
         """
-        The questions index page may display multiple questions.
+        The questions index page may display multiple active quizzes.
         """
-        create_quiz(quiz_name="Past quizzes.", days=-30)
-        create_quiz(quiz_name="Past quizzes.", days=-5)
+        create_quiz(quiz_name="Awake quizzes 1.", days=-30, active_level=True)
+        create_quiz(quiz_name="Awake quizzes 2.", days=-5, active_level=True)
         response = self.client.get(reverse('quizzes:index'))
         self.assertQuerysetEqual(
             response.context['latest_quiz_list'],
-            ['<Quiz: Past quizzes 2.>', '<Quiz: Past quizzes 1.>']
+            ['<Quiz: Awake quizzes 2.>', '<Quiz: Awake quizzes 1.>']
         )
 
 
 class QuizDetailViewTests(TestCase):
-    def test_future_quiz(self):
+    def test_sleep_quiz(self):
         """
-        The detail view of quizzes with a pub_date in the future
-        returns a 404 not found.
+        The detail view of inactive quizzes returns a 404 not found.
         """
-        future_quiz = create_quiz(quiz_name='Future quizzes.', days=5)
-        url = reverse('quizzes:quiz_detail', args=(future_quiz.id,))
+        sleep_quiz = create_quiz(quiz_name='Sleep quizzes.', days=-5, active_level=False)
+        url = reverse('quizzes:quiz_detail', args=(sleep_quiz[0].id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
-    def test_past_quiz(self):
+    def test_awake_quiz(self):
         """
-        The detail view of quizzes with a pub_date in the past
-        displays the quizzes's name.
+        The detail view of active quizzes returns the quiz nanme.
         """
-        past_quiz = create_quiz(quiz_name='Past quizzes.', days=-5)
-        url = reverse('polls:detail', args=(past_quiz.id,))
+        past_quiz = create_quiz(quiz_name='Awake quizzes.', days=-5, active_level=True)
+        url = reverse('quizzes:quiz_detail', args=(past_quiz[0].id,))
         response = self.client.get(url)
-        self.assertContains(response, past_quiz.quiz_name)
+        self.assertContains(response, past_quiz[0].name)
+
+
+class TakingQuizVTests(TestCase):
+    def test_new_quiz(self):
+        """
+        The start_new_quiz function should redirect to take_quiz when run.
+        :return:
+        """
+        quiz = create_quiz(quiz_name="test quiz", days=-5, active_level=True)
+        url = reverse('quizzes:start_new_quiz', args=(quiz[0].id, quiz[1].id))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_take_quiz(self):
+        """
+        """
+        quiz = create_quiz(quiz_name="test quiz", days=-5, active_level=True)
+        url = reverse('quizzes:take_quiz', args=(quiz[0].id, quiz[1].id, quiz[2].id))
+        response = self.client.get(url)
+        self.assertContains(response, quiz[0].name)
+
+
+class CreatingUserResponses(TestCase):
+    def test_create_new_response(self):
+        quiz = create_quiz(quiz_name="test quiz", days=-5, active_level=True)[0]
+        responseid = create_user_response(quiz.id)
+        userresponse = UserResponse.objects.filter(response_id=responseid)[0]
+        self.assertEqual(userresponse.parent_quiz, quiz)
